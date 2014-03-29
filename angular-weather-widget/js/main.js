fff72e78b73b03939d7a9432840d0c79c7eae038
pappu687 <pappu687@gmail.com>
2014-03-29 17:08:17 +0600

    Finished the app for Initial Demo.

--- angular-weather-widget/js/main.js
+++ angular-weather-widget/js/main.js
@@ -7,7 +7,7 @@ App.config(['$routeProvider', '$locationProvider',
       controller: 'GetWeatherCtrl'
     });
   }
-  ])
+])
 
 
 var weatherControllers = angular.module("weatherControllers", []);
@@ -15,39 +15,44 @@ weatherControllers.controller("AppController", ['$route', '$routeParams', '$loca
   function($route, $routeParams, $location) {
 
   }
-  ]);
+]);
 
 weatherControllers.controller("GetWeatherCtrl", ['$scope', 'weatherApi',
   function($scope, weatherApi) {
-    weatherApi.getWeeklyWeather().then(function(response){
-      $scope.data=response.data;
-      if($scope.data.list.length){
-        $scope.data.list.forEach(function(i, v){          
-          var date=moment(i.dt*1000);           
-          i.dt={
-            day:date.format("ddd")
-          };          
-          if(moment().format("d")==date.format("d")){ 
-            i.dt.today=true;
-          }
-        });
-      }
+    $scope.currentTime = moment().format('h:mm a');
+    weatherApi.getLocation().then(function(res) {            
+      weatherApi.getWeeklyWeather(res.data.city).then(function(response) {
+        $scope.data = response.data;
+        if ($scope.data.list.length) {
+          $scope.data.list.forEach(function(i, v) {
+            var date = moment(i.dt * 1000);
+            i.dt = {
+              day: date.format("ddd")
+            };
+            if (moment().format("d") == date.format("d")) {
+              i.dt.today = true;
+            }
+          });
+        }
+      });
     });
   }
-  ]);
+]);
 
 var weatherServices = angular.module('weatherServices', []);
 
 weatherServices.factory('weatherApi', ['myHttp',
   function(myHttp) {
     return {
-      getWeeklyWeather: function() {
-        return myHttp.getLocal('js/data/data.json');
-        // return myHttp.get('http://api.openweathermap.org/data/2.5/forecast/daily?q=Dhaka&mode=json');
+      getLocation: function() {
+        return myHttp.jsonp("http://muslimsalat.com/daily.json?callback=JSON_CALLBACK");
+      },
+      getWeeklyWeather: function(city) {        
+        return myHttp.get('http://api.openweathermap.org/data/2.5/forecast/daily?q='+city+'&mode=json&units=metric');
       }
     }
   }
-  ]);
+]);
 
 
 weatherServices.factory('myHttp', ['$http', 'myCache',
@@ -55,26 +60,30 @@ weatherServices.factory('myHttp', ['$http', 'myCache',
 
     var headers = {
       'cache': myCache,
-      'dataType': 'json'      
+      'dataType': 'json'
     };
-    var APPID="bc1e24c531732375aece237bb2a5d49a";
+    var APPID = "bc1e24c531732375aece237bb2a5d49a";
     return {
       config: headers,
       get: function(url, success, fail) {
-        return $http.get(url+"&APPID="+APPID, this.config);
+        return $http.get(url + "&APPID=" + APPID, this.config);
       },
       getLocal: function(url, success, fail) {
         return $http.get(url);
       },
       jsonp: function(url, success, fail) {
-        return $http.jsonp(url+"&APPID="+APPID, this.config);
+        return $http.jsonp(url, this.config);
       }
     };
   }
-  ]);
+]);
 
 weatherServices.factory('myCache', function($cacheFactory) {
   return $cacheFactory('myCache', {
     capacity: 100
   });
 });
+
+function JSON_CALLBACK(){
+  // Nothing
+}
